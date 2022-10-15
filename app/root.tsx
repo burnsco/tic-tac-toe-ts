391e2c20316c36f6f48e73ecd40d299cabf04502
Corey Burns <coreymburns@gmail.com>
2022-10-15 13:52:07 -0400

    basic board setup from reacte website

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,31 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
+import darkStylesUrl from "~/styles/dark.css";
+import globalStylesUrl from "~/styles/global.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: globalStylesUrl },
+    {
+      rel: "stylesheet",
+      href: darkStylesUrl,
+      media: "(prefers-color-scheme: dark)",
+    },
+  ];
+};
 
 export default function App() {
   return (
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,70 @@
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { useState } from "react";
+import stylesUrl from "~/styles/tictac.css";
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Tic Tac Toe - Typescript React",
+  };
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+
+function Square () {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+
+
+function renderSquare(i: number) {
+  return <Square />;
 }
+
+function Board() {
+  const [gameBoard, setGameBoard] = useState(Array(9).fill(null))
+
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+
+
+export default function TicTacToeGame(){
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
